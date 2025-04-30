4036124a03c49d780cbae872551c002a5e3d517e
knightx4 <ckloug4@gmail.com>
2025-04-30 06:27:43 -0400

    Fix type mismatch for onEquilibriumChange callback

--- app/page.tsx
+++ app/page.tsx
@@ -71,7 +71,7 @@ export default function HomePage() {
               <div>
                 <ISLMChart
                   params={params}
-                  onEquilibriumChange={(output: number) => setEquilibriumOutput(output)}
+                  onEquilibriumChange={(output: number | null) => setEquilibriumOutput(output)}
                 />
               </div>
               <div>
